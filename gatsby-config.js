55ebf85c60249fc32dad2459b277d63c66275408
Will Worth <will.worth@gmail.com>
2018-02-21 15:09:05 +0100

    add sass plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,6 @@ module.exports = {
   siteMetadata: {
     title: "Will's first Gatsby site",
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: ['gatsby-plugin-react-helmet',
+            'gatsby-plugin-sass'],
 };
