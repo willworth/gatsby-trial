a1b9b53f5da726fb915102908c530a688f28206b
Will Worth <will.worth@gmail.com>
2018-02-23 15:21:11 +0100

    add page templating

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,44 @@
 /**
  * Implement Gatsby's Node APIs in this file.
- *
+ *https://www.gatsbyjs.org/tutorial/part-four/#create-a-list-of-our-sites-markdown-files-in-srcpagesindexjs
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
  // You can delete this file if you're not using it
+
+
+ const path = require('path');
+
+ exports.createPages = ({boundActionCreators, graphql}) => {
+   const {createPage} = boundActionCreators;
+ 
+   const postTemplate = path.resolve('src/templates/post.js');  
+ 
+   return graphql(`{
+     allMarkdownRemark {
+       edges {
+         node {
+           html
+           id
+           frontmatter {
+             path
+             title
+           }
+         }
+       }
+     }
+   }`)
+   .then(res => {
+     if(res.errors) {
+       return Promise.reject(res.errors);
+     }
+ 
+     res.data.allMarkdownRemark.edges.forEach(({node}) => {
+       createPage({
+         path: node.frontmatter.path,
+         component: postTemplate
+       })
+     })
+ 
+   })
+ }
--- src/pages/23-2-18-Hello-World/index.md
+++ src/pages/23-2-18-Hello-World/index.md
@@ -4,3 +4,6 @@ title: 'Hello World!'
 ---
 
 # Hello World!
+## I'm just using this starter page to trial some markdown.
+
+[Handy markdown pdf](https://guides.github.com/pdfs/markdown-cheatsheet-online.pdf)
